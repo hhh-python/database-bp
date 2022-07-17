b64a31a6189b15dbb7f1bf79a9cc50ba34a6b315
HabenGebremeskel <hhh.python@gmail.com>
2022-07-17 12:10:04 -0400

    add book to db function

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from optparse import OptionGroup
 from tkinter import Menu
+from turtle import title
 from models import (Base, session, 
                     Book, engine)
 import datetime 
 import csv
+import time
 
 
 
@@ -34,14 +36,34 @@ def clean_date(date_str):
               'May', 'June', 'July', 'August', 'September', 
               'October', 'November', 'December']
     split_date = date_str.split(' ')
-    month = int(months.index(split_date[0]) + 1)
-    day = int(split_date[1].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year, month, day)
+    try:
+        month = int(months.index(split_date[0]) + 1)
+        day = int(split_date[1].split(',')[0])
+        year = int(split_date[2])
+        return_date = datetime.date(year, month, day)
+    except ValueError:
+        input('''
+         \n***** DATE ERROR ******
+         \rThe date format should include a valid Month Day, Year from the past.
+         \rEx: January 13, 2013
+         \rPress enter to try again.
+         \r*********************** ''')
+        return 
+    else:
+        return return_date
 
 def clean_price(price_str):
-    price_float = float(price_str)
-    return int(price_float * 100)
+    try:
+        price_float = float(price_str)
+    except ValueError:
+        input('''
+         \n***** PRICE ERROR ******
+         \rThe price should be a number without a currency symbol.
+         \rEx: 10.99
+         \rPress enter to try again.
+         \r*********************** ''')
+    else:
+        return int(price_float * 100)
 
 
 
@@ -66,7 +88,25 @@ def app():
     while app_running:
         choice = menu()
         if choice == '1':
-            pass
+            title = input('Title: ')
+            author = input('Author: ')
+            date_error = True
+            while date_error:
+                date = input('Publshed (Ex: October 25, 2017): ')
+                date = clean_date(date)
+                if type(date) == datetime.date:
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input('Price (Ex: 25.64): ')
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author, published_date=date, price=price)
+            session.add(new_book)
+            session.commit()
+            print('--- Book added! ---')
+            time.sleep(1.5)
         elif choice == '2':
             pass
         elif choice == '3':
@@ -82,4 +122,5 @@ def app():
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    add_csv()
+    add_csv
+    app()
