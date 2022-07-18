e2fcc052abb1999010877f12ef87a415199d63dd
HabenGebremeskel <hhh.python@gmail.com>
2022-07-18 16:20:27 -0400

    able to view books

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def clean_date(date_str):
          \rEx: January 13, 2013
          \rPress enter to try again.
          \r*********************** ''')
-        return 
+        return
     else:
         return return_date
 
@@ -62,8 +62,35 @@ def clean_price(price_str):
          \rEx: 10.99
          \rPress enter to try again.
          \r*********************** ''')
+        return
     else:
         return int(price_float * 100)
+
+
+
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input('''
+            \n***** ID ERROR ******
+            \rThe id should be a number.
+            \rPress enter to try again.
+            \r*********************** ''')
+        return
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f'''
+            \n***** ID ERROR ******
+            \rOptions {options}.
+            \rPress enter to try again.
+            \r*********************** ''')
+            return 
+
+
+
 
 
 
@@ -112,7 +139,26 @@ def app():
                 print(f'{book.id} | {book.title} | {book.author}')
             input('\nPress enter to return to the main menu.')
         elif choice == '3':
-            pass 
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f''' 
+                    \rId Options: {id_options}
+                    \rBook Id: ''')
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f'''
+            \r*-----------------------------------------------------* 
+            \r{the_book.title} by {the_book.author} 
+            \rPublished: {the_book.published_date}
+            \rPrice: ${the_book.price / 100}
+            \r*-----------------------------------------------------*
+            ''')
+            input('\nPress enter to return to main menu ')
         elif choice == '4':
             pass
         else:
